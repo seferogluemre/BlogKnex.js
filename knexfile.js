d4db727145d1d1cb5d2e33b729b96d483bcb8c9d
seferogluemre <yunusemre5331@icloud.com>
2025-03-02 11:45:33 +0300

    knexfile config update

---
+++ knexfile.js
@@ -0,0 +1,26 @@
+// Update with your config settings.
+
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+
+import dotenv from 'dotenv'
+
+dotenv.config();
+
+export default {
+
+  development: {
+    client: 'postgresql',
+    connection: {
+      database: process.env.DB_NAME,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+    },
+    migrations: {
+      directory: './src/migrations'
+    }
+  },
+};
