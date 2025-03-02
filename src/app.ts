fe947c7d8fa492f59406b07a9839bac43d6f8dcc
seferogluemre <yunusemre5331@icloud.com>
2025-03-02 11:32:53 +0300

    first commit

---
+++ src/app.ts
@@ -0,0 +1,12 @@
+import express from 'express'
+
+const app = express()
+require('dotenv').config()
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log(`Example app listening on port ${process.env.PORT}`)
+})
